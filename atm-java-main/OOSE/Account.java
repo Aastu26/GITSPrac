f30a6a9b25b803147a4f75f5a78d9c937ad1cd44
Aastu26 <97172747+Aastu26@users.noreply.github.com>
2023-01-03 22:13:12 +0530

    Add files via upload

---
+++ atm-java-main/OOSE/Account.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+public class Account extends Person {
+
+    private String accountPin;
+    private String accountId;
+    private String accountName;
+    private int accountBalance;
+    private boolean accountInfo;
+    private boolean accountManager;
+    private Manager manager;
+
+    public Account(String accountName, String accountId, String accountPin, int accountBalance) {
+        this.accountPin = accountPin;
+        this.accountId = accountId;
+        this.accountName = accountName;
+        this.accountBalance = accountBalance;
+    }
+
+    public String getAccountPin() {
+        return accountPin;
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public int getAccountBalance() {
+        return accountBalance;
+    }
+
+    public boolean getaccountInfo() {
+        return accountInfo;
+    }
+
+    public boolean getAccountManager() {
+        return accountManager;
+    }
+
+    public void setAccountBalance(int balance) {
+        this.accountBalance = balance;
+    }
+
+    public void setaccountInfo(boolean accountInfo) {
+        this.accountInfo = accountInfo;
+    }
+
+    public void setManager(String managerId) {
+        Manager[] 
+        manager = new Manager[99];
+        Scanner inputAccountId = new Scanner(System.in);
+        manager[0] = new Manager();
+        manager[0].setManager(managerId);
+        manager[0].setHaveManager(true); 
+        manager[0].setAccount(accountId, accountPin);
+        this.accountManager = true;
+        System.out.println("Manager ID: " + manager[0].getManagerId());
+    }
+
+    public void getManagerInformation() {
+        System.out.println("Manager ID: " + manager.getManagerId());
+        System.out.println("Manager Password: " + manager.getManagerPassword());
+        System.out.println("Have Manager: " + manager.getHaveManager());
+    }
+
+}
+
